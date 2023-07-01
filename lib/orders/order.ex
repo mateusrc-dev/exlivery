e9de8f6cf2a8c0d98c9a3023a6857d35088581e3
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-01 17:36:50 -0300

    updated struct order

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -1,11 +1,35 @@
 defmodule Exlivery.Orders.Order do
+  alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
   @keys [:user_cpf, :delivery_address, :items, :total_price]
 
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build do
-    {:ok, %__MODULE__{user_cpf: nil, delivery_address: nil, items: nil, total_price: nil}}
+  # we will receive the item, if it is not passed we will put the struct of the items
+  def build(%User{cpf: cpf, address: address}, [%Item{} | _items] = items) do
+    {:ok,
+     %__MODULE__{
+       user_cpf: cpf,
+       delivery_address: address,
+       items: items,
+       total_price: calculate_total_price(items)
+     }}
+  end
+
+  def build(_user, _items) do
+    {:error, "Invalid parameters"}
+  end
+
+  defp calculate_total_price(items) do
+    Enum.reduce(items, Decimal.new("0.00"), fn item, acc -> sum_prices(item, acc) end)
+  end
+
+  defp sum_prices(%Item{unity_price: price, quantity: quantity}, acc) do
+    price
+    |> Decimal.mult(quantity)
+    |> Decimal.add(acc)
   end
 end
