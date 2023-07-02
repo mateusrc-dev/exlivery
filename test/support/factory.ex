75bd578efa343b82029ef3f79c7212185db5280b
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-01 21:33:27 -0300

    created test to struct Order

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -19,6 +19,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      user_cpf: "123456789",
+      delivery_address: "Rua das raparigas",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50")
     }
   end
 end
