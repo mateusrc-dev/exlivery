d84f7f77eda42b8073b0fe701514177ca8f4fc83
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-01 21:08:41 -0300

    created test for struct item

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
   alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
 
   def user_factory do
     %User{
@@ -9,6 +10,15 @@ defmodule Exlivery.Factory do
       email: "mateus@bananas.com",
       cpf: "123456789",
       age: 28
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de peperoni",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
     }
   end
 end
